9b6d194bd53cc9d3ac9bc9fd9ee1e215d1b30a7f
laszlocseh <csehlaszlo91@gmail.com>
2018-12-05 11:45:50 +0200

    Fixes in questions text

--- src/wise/msfd/translation/views.py
+++ src/wise/msfd/translation/views.py
@@ -210,8 +210,8 @@ class TranslationView(BrowserView):
     def translate(self, source_lang, value):
         # TODO: implement getting the translation from annotations
 
-        show_translation = (self.can_modify() and
-                            len(unicode(value).split(' ')) > 4)
+        is_long_text = len(unicode(value).split(' ')) > 4
+        show_translation = self.can_modify() and is_long_text
 
         if not value:
             return self.translate_snip(text=value,
