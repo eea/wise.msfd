d76c4895c6a4d6c4b5ad3bdadf4fcf120fa1ff26
laszlocseh <csehlaszlo91@gmail.com>
2019-07-25 11:41:37 +0300

    WiP compliance module start page nav

--- src/wise/msfd/compliance/main.py
+++ src/wise/msfd/compliance/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from itertools import chain
+
 from wise.msfd.gescomponents import GES_DESCRIPTORS
 
 from .base import BaseComplianceView
@@ -15,3 +17,35 @@ class DescriptorsView(BaseComplianceView):
     @property
     def descriptors(self):
         return GES_DESCRIPTORS
+
+
+class StartComplianceView2(BaseComplianceView):
+    name = 'comp-start2'
+
+    def get_folder_by_id(self, id):
+        folders = [
+            x.contentValues()
+            for x in self.context.contentValues()
+            if x.portal_type == 'Folder'
+               and x.id == id
+        ]
+        folders = [f for f in chain(*folders)]
+
+        return folders
+
+    @property
+    def regional_descriptors_folders(self):
+        id = 'regional-descriptors-assessments'
+        folders = self.get_folder_by_id(id)
+
+        return folders
+
+    @property
+    def national_descriptors_folders(self):
+        id = 'national-descriptors-assessments'
+        folders = self.get_folder_by_id(id)
+
+        return folders
+
+    def __call__(self):
+        return self.index()
