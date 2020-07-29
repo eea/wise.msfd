95643b1f72dd3cba164a3d01ecbaa17ed2bdeacb
laszlocseh <csehlaszlo91@gmail.com>
2020-07-29 16:14:47 +0300

    Regional descriptors viewlets

---
+++ src/wise/msfd/compliance/regionaldescriptors/viewlets.py
@@ -0,0 +1,80 @@
+from plone.app.layout.viewlets.common import TitleViewlet
+
+from ..regionalsummary.base import BaseRegSummaryView
+from .base import BaseRegComplianceView
+from .main import (RegDescRegionOverviewReports, RegDescRegionOverviewAssessments)
+
+
+class ReportTitleViewlet(TitleViewlet, BaseRegComplianceView):
+    @property
+    def page_title(self):
+        params = {
+            'article': self.article,
+            'year': self.view.year,
+            'descriptor': self.descriptor_title,
+            'region': self.country_region_name,
+        }
+
+        return (u'MS/{article}/{year}/{descriptor}/'
+                u'{region}'.format(**params))
+
+
+class AssessmentEditTitleViewlet(TitleViewlet, BaseRegComplianceView):
+    @property
+    def page_title(self):
+        params = {
+            'article': self.article,
+            'descriptor': self.descriptor_title,
+            'region': self.country_region_name,
+        }
+
+        return (u"Edit COM/{article}/2018/{descriptor}/"
+                u"{region}".format(**params))
+
+
+class ArticleTitleViewlet(TitleViewlet, BaseRegComplianceView):
+
+    @property
+    def page_title(self):
+        params = {
+            'article': self.article,
+            'descriptor': self.descriptor_title,
+            'region': self.country_region_name,
+        }
+
+        return (u'COM/{article}/2018/{descriptor}/'
+                u'{region}'.format(**params))
+
+
+class SummaryRegionTitleViewlet(TitleViewlet, BaseRegSummaryView):
+    @property
+    def page_title(self):
+        params = {
+            'region': self.region_name
+        }
+
+        return u'COM/Art12/2018/{region}-Summary'.format(**params)
+
+
+class RegionStartReportsTitleViewlet(TitleViewlet,
+                                     RegDescRegionOverviewReports):
+
+    @property
+    def page_title(self):
+        params = {
+            'region': self.country_region_name
+        }
+
+        return u'MS/{region}'.format(**params)
+
+
+class RegionStartAssessmentsTitleViewlet(TitleViewlet,
+                                         RegDescRegionOverviewAssessments):
+
+    @property
+    def page_title(self):
+        params = {
+            'region': self.country_region_name
+        }
+
+        return u'COM/{region}'.format(**params)
