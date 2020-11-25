ae97742cb7f29e6532fe363d80b65ccf3ab6ac0f
laszlocseh <csehlaszlo91@gmail.com>
2020-11-25 17:13:20 +0200

    WiP National overview page

--- src/wise/msfd/compliance/nationalsummary/overview.py
+++ src/wise/msfd/compliance/nationalsummary/overview.py
@@ -5,51 +5,27 @@ import logging
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
 
-from wise.msfd.data import get_report_filename
+from wise.msfd.compliance.utils import group_by_mru
+from wise.msfd.data import (get_all_report_filenames,
+                            get_envelope_release_date, get_factsheet_url,
+                            get_report_file_url, get_report_filename,
+                            get_xml_report_data)
 from wise.msfd.translation import get_translated, retrieve_translation
 from wise.msfd.utils import (ItemList, TemplateMixin, db_objects_to_dict,
-                             fixedorder_sortkey, timeit)
+                             fixedorder_sortkey, items_to_rows, timeit)
 
-from ..nationaldescriptors.a7 import Article7
-from ..nationaldescriptors.a34 import Article34
-from ..nationaldescriptors.base import BaseView
+from ..nationaldescriptors.data import get_report_definition
+from ..nationaldescriptors.reportdata import ReportData2018Secondary
+from ..nationaldescriptors.proxy import Proxy2018
 from .base import BaseNatSummaryView
+
 
 logger = logging.getLogger('wise.msfd')
 
 
-class Article34Copy(Article34):
-    """ Class to override the template """
-    template = ViewPageTemplateFile('pt/report-data-secondary.pt')
-    title = "Articles 3 & 4 Marine regions"
-
-
-class Article7Copy(Article7):
-    """ Class to override the template """
-    template = ViewPageTemplateFile('pt/report-data-secondary.pt')
-    title = "Article 7 Competent authorities"
-
-
-class ArticleTable(BaseView):
-    impl = {
-        'Art3': Article34Copy,
-        'Art4': Article34Copy,
-        'Art7': Article7Copy,
-    }
-
-    is_translatable = True
-
-    def __init__(self, context, request, article):
-        super(ArticleTable, self).__init__(context, request)
-
-        self._article = article
-        self.klass = self.impl[article]
-
-    year = '2012'
-
-    @property
-    def article(self):
-        return self._article
+class ReportData2018SecondaryOverview(ReportData2018Secondary,
+                                      BaseNatSummaryView):
+    is_translatable = False
 
     @property
     def descriptor(self):
@@ -63,32 +39,87 @@ class ArticleTable(BaseView):
     def country_region_code(self):
         return 'No region'
 
-    def get_article_title(self, klass):
-        tmpl = u"<h4>{}</h4>"
-        title = klass.title
+    def render_reportdata(self):
+        """
+        1. Get all reported files under Article 7 or 3/4
+        2. Render the data separately for all files
+        3. Concat the rendered htmls into a single
 
-        return tmpl.format(title)
+        :return: rendered html
+        """
 
-    def get_report_filename(self, art=None):
-        # needed in article report data implementations, to retrieve the file
+        template = self.get_template(self.article)
+        urls = get_all_report_filenames(self.country_code, self.article)
 
-        return get_report_filename(
-            self.year, self.country_code, self.country_region_code,
-            art or self.article, self.descriptor
-        )
+        rendered_results = []
+
+        for (index, url) in enumerate(urls[:1]):
+            prev_url = None
+            view = self.get_implementation_view(url, prev_url)
+            report = self.get_report_metadata_from_view(view, url)
+
+            # Report Header
+            report_by = None
+            report_date = get_envelope_release_date(url)
+
+            if report:
+                report_by = report.ContactOrganisation
+                # report_date = report.ReportingDate
+
+            res = []
+            source_file = (url.rsplit('/', 1)[-1], url + '/manage_document')
+            factsheet = get_factsheet_url(url)
+
+            view()      # updates the view
+            data = [Proxy2018(row, self) for row in view.cols]
+
+            if self.article == 'Art7':
+                data_by_mru = group_by_mru(data)
+            else:
+                data_by_mru = {'no mru': data}
+
+            fields = get_report_definition(self.article).get_fields()
+
+            for mru, rows in data_by_mru.items():
+                _rows = items_to_rows(rows, fields)
+
+                res.append((mru, _rows))
+
+            report_header = self.report_header_template(
+                title=(index == 0 and self.report_header_title or ''),
+                factsheet=factsheet,
+                report_by=report_by,
+                source_file=source_file,
+                report_due=None,
+                report_date=report_date.date(),
+                help_text=self.help_text,
+                multiple_source_files=False,
+                show_navigation=index == 0,
+            )
+
+            rendered_results.append(template(data=res,
+                                             report_header=report_header,
+                                             show_navigation=False))
+
+        res = "<hr/>".join(rendered_results)
+
+        return res or "No data found"
+
 
     def __call__(self):
-        try:
-            self.view = self.klass(
-                self, self.request, self.country_code,
-                self.country_region_code, self.descriptor, self.article,
-                self.muids
-            )
-            rendered_view = self.view()
-        except:
-            rendered_view = 'Error getting report'
+        return self.render_reportdata()
 
-        return self.get_article_title(self.klass) + rendered_view
+
+class Article7Table(ReportData2018SecondaryOverview):
+    @property
+    def article(self):
+        return 'Art7'
+
+
+class Article34Table(ReportData2018SecondaryOverview):
+    @property
+    def article(self):
+        return 'Art4'
 
 
 class NationalOverviewView(BaseNatSummaryView):
@@ -106,25 +137,23 @@ class NationalOverviewView(BaseNatSummaryView):
     # @cache(get_reportdata_key, dependencies=['translation'])
     @timeit
     def render_reportdata(self):
-        report_header = self.report_header_template(
+        report_header = self.overview_header_template(
             title="National summary report: {}".format(
                 self.country_name,
             )
         )
         self.tables = [
             report_header,
-            # ArticleTable(self, self.request, 'Art7'),
-            # ArticleTable(self, self.request, 'Art3-4'),
+            Article7Table(self.context, self.request)(),
+            Article34Table(self.context, self.request)(),
             # trans_edit_html,
         ]
 
-        template = self.template
-
-        return template(tables=self.tables)
+        return self.template(tables=self.tables)
 
     def __call__(self):
         if 'edit-data' in self.request.form:
-            url = "{}/edit".format(self.absolute_url())
+            url = "{}/edit".format(self.context.absolute_url())
 
             return self.request.response.redirect(url)
 
