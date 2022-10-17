b20539a6bd08ede57d9dc79bed365544f815ab7e
laszlocseh <csehlaszlo91@gmail.com>
2022-10-17 14:25:57 +0300

    Fix error in assessment edit

--- src/wise/msfd/compliance/nationaldescriptors/assessment.py
+++ src/wise/msfd/compliance/nationaldescriptors/assessment.py
@@ -107,7 +107,11 @@ class EditAssessmentDataForm(BaseView, EditAssessmentDataFormMain):
             return default
 
         # parse the datestring to reformat into a clearer format
-        local_time = datetime.datetime.strptime(local_time, '%b %d %Y %I:%M %p')
+        try:
+            local_time = datetime.datetime.strptime(local_time, '%b %d %Y %I:%M %p')
+        except:
+            local_time = datetime.datetime.strptime(local_time, '%b %d, %Y %I:%M %p')
+
         local_time = datetime.datetime.strftime(local_time, "%Y-%m-%d %H:%M")
 
         return local_time
