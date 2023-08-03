90bf9fa7881c295603ae25f9c6b9c68cc3f97de8
andreiggr <andrei.ggr@gmail.com>
2023-08-03 16:29:00 +0300

    chore: SPMeasure fields are list now #254114

--- src/wise/msfd/interfaces.py
+++ src/wise/msfd/interfaces.py
@@ -3,11 +3,7 @@ from zope.interface import Attribute, Interface
 from zope.schema import Choice, List  # Int, Text, TextLine
 from plone.supermodel import model
 from plone.autoform.interfaces import IFormFieldProvider
-from zope.schema import Text
 from zope.interface import provider
-
-# from plone.app.textfield import RichText
-
 
 class IMainForm(Interface):
     """ A marker interface to easily identify main forms
@@ -47,38 +43,38 @@ class IMarineUnitIDsSelect(Interface):
 class ISPMeasureFields(model.Schema):
     """Fields for SPMeasure """
 
-    measure_name = Text(title=u"Measure name", description=u"", required=False)
-    sector = Text(title=u"Sector", description=u"", required=False)
-    use = Text(title=u"Use or activity", description=u"", required=False)
-    origin = Text(title=u"Origin of the measure",
+    measure_name = List(title=u"Measure name", description=u"", required=False)
+    sector = List(title=u"Sector", description=u"", required=False)
+    use = List(title=u"Use or activity", description=u"", required=False)
+    origin = List(title=u"Origin of the measure",
                   description=u"", required=False)
-    nature = Text(title=u"Nature of the measure",
+    nature = List(title=u"Nature of the measure",
                   description=u"", required=False)
-    status = Text(title=u"Status", description=u"", required=False)
-    impacts = Text(title=u"Measure impacts to",
+    status = List(title=u"Status", description=u"", required=False)
+    impacts = List(title=u"Measure impacts to",
                    description=u"", required=False)
-    impacts_further_details = Text(
+    impacts_further_details = List(
         title=u"Measure impacts to, further details", description=u"", required=False)
-    water_body_cat = Text(title=u"Water body category",
+    water_body_cat = List(title=u"Water body category",
                           description=u"", required=False)
-    spatial_scope = Text(title=u"Spatial scope",
+    spatial_scope = List(title=u"Spatial scope",
                          description=u"", required=False)
-    country_coverage = Text(title=u"Country coverage",
+    country_coverage = List(title=u"Country coverage",
                             description=u"", required=False)
 
     # Further info
-    measure_purpose = Text(title=u"Measure purpose",
+    measure_purpose = List(title=u"Measure purpose",
                            description=u"", required=False)
-    measure_type = Text(title=u"Measure type", description=u"", required=False)
-    measure_location = Text(title=u"Measure location",
+    measure_type = List(title=u"Measure type", description=u"", required=False)
+    measure_location = List(title=u"Measure location",
                             description=u"", required=False)
-    measure_response = Text(title=u"Measure response",
+    measure_response = List(title=u"Measure response",
                             description=u"", required=False)
-    measure_additional_info = Text(
+    measure_additional_info = List(
         title=u"Measure additional info", description=u"", required=False)
-    pressure_type = Text(title=u"Type of pressure",
+    pressure_type = List(title=u"Type of pressure",
                          description=u"", required=False)
-    pressure_name = Text(title=u"Pressure name",
+    pressure_name = List(title=u"Pressure name",
                          description=u"", required=False)
-    ranking = Text(title=u"Ranking", description=u"", required=False)
-    season = Text(title=u"Season", description=u"", required=False)
+    ranking = List(title=u"Ranking", description=u"", required=False)
+    season = List(title=u"Season", description=u"", required=False)
