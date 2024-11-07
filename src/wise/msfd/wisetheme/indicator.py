dad9fe3472b93622e7ee7bb153a6487069410a49
laszlocseh <csehlaszlo91@gmail.com>
2024-11-07 15:40:13 +0200

    fix: added script to fix Indicators last update field

---
+++ src/wise/msfd/wisetheme/indicator.py
@@ -0,0 +1,50 @@
+""" Indicators """
+from zope.interface import alsoProvides
+from plone.protect.interfaces import IDisableCSRFProtection
+from Products.Five.browser import BrowserView
+from DateTime import DateTime
+
+
+class UpdateIndicatorLastUpdate(BrowserView):
+    def update_indicators(self):
+        updated_items = []
+
+        # Access the portal catalog
+        catalog = self.context.portal_catalog
+
+        # Search for all items of type "indicator"
+        indicators = catalog(portal_type="indicator")
+
+        # Process each indicator
+        for brain in indicators:
+            obj = brain.getObject()
+            current_value = getattr(obj, 'indicator_last_update', None)
+
+            if current_value:
+                if isinstance(current_value, str):
+                    # If it's a string, split by space and keep only the date part
+                    date_only = current_value.split(" ")[0]
+                elif isinstance(current_value, DateTime):
+                    # If it's a DateTime object, convert to YYYY-MM-DD format
+                    date_only = current_value.strftime('%Y-%m-%d')
+                else:
+                    date_only = current_value
+
+                # import pdb; pdb.set_trace()
+                # Update the field
+                obj.indicator_last_update = date_only
+                obj.reindexObject()
+
+                updated_items.append(obj.absolute_url())
+
+        return updated_items
+
+    def __call__(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
+
+        updated_items = self.update_indicators()
+
+        if updated_items:
+            return f"Updated {len(updated_items)} items: {', '.join(updated_items)}"
+        else:
+            return "No items to update."
