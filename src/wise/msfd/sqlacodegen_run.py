2cbe3f7954d9d9db3d602137cb4c710a5ece55d3
laszlocseh <csehlaszlo91@gmail.com>
2025-07-23 15:29:11 +0300

    Added sqlacodegen venv and script

---
+++ src/wise/msfd/sqlacodegen_run.py
@@ -0,0 +1,26 @@
+
+import subprocess
+import os
+
+env = os.environ.get
+CRESTEDDUCK_HOST = env('CRESTEDDUCK_HOST', '')
+CRESTEDDUCK_DOMAIN = env('CRESTEDDUCK_DOMAIN', '')
+CRESTEDDUCK_USER = env('CRESTEDDUCK_USER', '')
+CRESTEDDUCK_PASSWORD = env('CRESTEDDUCK_PASSWORD', '')
+
+dsn = ("mssql+pymssql://{}\\{}:{}@{}:1433".format(
+        CRESTEDDUCK_DOMAIN,
+        CRESTEDDUCK_USER,
+        CRESTEDDUCK_PASSWORD,
+        CRESTEDDUCK_HOST)
+)
+
+cmd = [
+    "sqlacodegen",
+    "--outfile", "sql2024.py",
+    "--schema=data",
+    "{}/MSFD2024_public".format(dsn),
+]
+
+
+subprocess.run(cmd, check=True)
