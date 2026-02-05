43ea9bc27cce5c21f12d1f039267bef387c13313
laszlocseh <csehlaszlo91@gmail.com>
2026-02-05 14:37:02 +0200

    migrate assessment module templates to plone 6

--- src/wise/msfd/compliance/base.py
+++ src/wise/msfd/compliance/base.py
@@ -1,4 +1,4 @@
-#pylint: skip-file
+# pylint: skip-file
 from __future__ import absolute_import
 import logging
 from collections import namedtuple
@@ -58,11 +58,12 @@ QUESTION_DISPLAY_IDS = {
     "Ad24J": "Ad24K",
 }
 
+
 def get_question_display_id(question_id):
     """ the question_id for some questions were changed and we cannot just change
         the question_id as it is used to store the data
         use this only to display a different question_id for the question """
-    
+
     return QUESTION_DISPLAY_IDS.get(question_id, question_id)
 
 
@@ -140,7 +141,7 @@ MAIN_FORMS = [Tab(*x) for x in [
 
     ('help',
      'help-section',
-     '<i class="glyphicon glyphicon-question-sign" />',
+     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>',
      '<span>&nbsp;</span>',
      'manage-users',
      '',
@@ -149,7 +150,7 @@ MAIN_FORMS = [Tab(*x) for x in [
 
     ('@@compliance-admin',
      'compliance-admin',
-     '<i class="glyphicon glyphicon-cog" />',
+     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16"> <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/> <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/> </svg>',
      '<span>&nbsp;</span>',
      'manage-users',
      '',
@@ -781,6 +782,7 @@ class BaseComplianceView(BrowserView, BasePublicPage, SecurityMixin):
 
             return obj
 
+
 def _a10_ids_cachekey(method, self, descriptor, **kwargs):
     muids = [m.id for m in kwargs['muids']]
     key = '{}-{}-{}'.format(
@@ -817,7 +819,7 @@ class AssessmentQuestionDefinition:
         self.klass = node.get('class')
         self.use_criteria = node.get('use-criteria')
         self.definition = u"{}: {}".format(
-            # get_question_display_id(self.id), 
+            # get_question_display_id(self.id),
             self.id,
             node.find('definition').text.strip())
         self.answers = [x.strip()
@@ -1036,7 +1038,7 @@ class AssessmentQuestionDefinition:
         if self.article in ['Art3', 'Art4']:
             res = filtered_descriptors(res, self)
         if self.article in ['Art13', 'Art14', 'Art1314CrossCutting',
-                'Art13Completeness', 'Art14Completeness']:
+                            'Art13Completeness', 'Art14Completeness']:
             res = filtered_descriptors(res, self)
 
         return sorted_criterions(res)
