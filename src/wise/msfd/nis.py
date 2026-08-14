8f37efc9c157c4c4e22f6d5981febbf53244cd18
laszlocseh <csehlaszlo91@gmail.com>
2026-08-14 10:33:22 +0300

    fix pylint

--- src/wise/msfd/nis.py
+++ src/wise/msfd/nis.py
@@ -2,8 +2,6 @@
 from functools import lru_cache
 from urllib.parse import urlparse, parse_qs
 
-from pkg_resources import resource_filename
-
 import logging
 import json
 import datetime
@@ -13,6 +11,8 @@ import os
 import six
 import transaction
 import xlsxwriter
+
+from pkg_resources import resource_filename
 
 from zExceptions import BadRequest
 from plone import api
